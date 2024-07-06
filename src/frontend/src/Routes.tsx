1d4eaadecd1d0a9975736ef74287bbf8078eac18
arunmopare <arunmopare546@gmail.com>
2024-07-06 12:07:24 +0530

    added routing and basic ui layer

---
+++ src/frontend/src/Routes.tsx
@@ -0,0 +1,18 @@
+import AboutUs from "./Pages/AboutUs";
+import CreateOrder from "./Pages/CreateOrder";
+import Home from "./Pages/Home";
+import Orders from "./Pages/Orders";
+
+interface RouteConfig {
+    path: string;
+    element: React.ReactNode;
+}
+
+const routes: RouteConfig[] = [
+    { path: '/', element: <Home /> },
+    { path: '/about-us', element: <AboutUs /> },
+    { path: '/orders', element: <Orders /> },
+    { path: '/create-order', element: <CreateOrder /> },
+];
+
+export default routes;
